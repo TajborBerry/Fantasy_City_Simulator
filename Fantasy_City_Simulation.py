bfcbbe7bbe725000f43d4e5f6012a264c3e336aa
Tibor Bogyó <tajbor.berry@gmail.com>
2022-06-07 18:52:43 +0100

    Names and stuff

--- Fantasy_City_Simulation.py
+++ Fantasy_City_Simulation.py
@@ -1,21 +1,19 @@
 import numpy as np
 import random
 
-print("Hello World")
-
-print("did my push work?")
-
-food = [1]*10
-
 class Citizen:
-    def __init__(self, l):
+    def __init__(self, l, parent = "God", FirstName = "Cool" , FamilyName = "Gang" ):
         self.str = l[0]
         self.dex = l[1]
         self.con = l[2]
         self.int = l[3]
         self.wis = l[4]
         self.cha = l[5]
-        self.l= l       
+        self.l= l
+        self.childlist = []
+        self.parent = parent
+        self.FirstName = FirstName
+        self.FamilyName = FamilyName     
     def statblock(self):
         print(" Strength: " + str(self.str) 
         + "\n Dexterity: " + str(self.dex)
@@ -23,36 +21,44 @@ class Citizen:
         + "\n Intelligence: " + str(self.int)
         + "\n Wisdom: " + str(self.wis)
         + "\n Charisma: " + str(self.cha))
+    def childlistadd(self,child):
+        self.childlist.append(child)
+    def print(self):
+        return str(self.FirstName)+ " " + str(self.FamilyName)
+    
 
-def growth(c1,c2):
+def mating(c1,c2):
     indices = random.sample(range(len(c1.l)),int(len(c1.l)/2))
     newpeepo = c1.l.copy()
     for i in range(int(len(c1.l))):
         if i in indices:
             newpeepo[i]= c2.l[i]
-    return Citizen(newpeepo)
+    return Citizen(newpeepo,parent=[c1.print(),c2.print()])
 
 
-def growth_old(c1,c2,x):
+def mating_old(c1,c2,x):
     return Citizen(list(c1.l[0:x] + c2.l[x:len(c1.l)]))
 
 
 
 l = [10,12,8,10,12,8]
 
-c1 = Citizen(l)
+c1 = Citizen(l,FirstName= "David", FamilyName= "Uhg")
 
 l = [11,11,11,11,11,11]
 
-c2 = Citizen(l)
+c2 = Citizen(l,FirstName= "Daniella", FamilyName= "Guh")
 
 random.randint(0, 5)
 
-c3 = growth_old(c1,c2,random.randint(0, 5))
+c3 = mating_old(c1,c2,random.randint(0, 5))
 c3.l
 
-c4 = growth(c1,c2)
+c4 = mating(c1,c2)
 c4.l
+
+c1.childlistadd(print(c4))
+c2.childlistadd(print(c4))
 
 
 '''
